08f4e117f54522283612082980985513851b4999
Deval027 <homeroand6@gmail.com>
2025-06-11 17:54:21 -0600

    fix path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div id="container">
       <Expectancy />
-      <Close id="question1" iconX="/_.png" question="question2"/>
+      <Close id="question1" iconX="/fisshy/_.png" question="question2"/>
       <p id="fissh">fissh</p>
       <img src='/doesfishpng.png' alt='entity' id="fishy" ></img>
       <Btn label="ok" />
